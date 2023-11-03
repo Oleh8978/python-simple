4e51735f7820b3eb1692b1172fcf7defe6614e17
Oleg8978 <mihajlovskyoleg@gmail.com>
2023-11-03 02:04:32 +0100

    commit and input variables

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 # constants
 
 description = Input('chose math operation (+, -, *, /)')
+
+# var naming standrts https://www.w3schools.com/python/gloss_python_variable_names.asp#:~:text=A%20variable%20name%20must%20start,AGE%20are%20three%20different%20variables)
+
+float first_number = input("First number: ")
+
+float number_to_add = input("Number to add : ")
