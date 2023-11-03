887c3338a242cbe7999bb0755b669f88e4742b29
Oleg8978 <mihajlovskyoleg@gmail.com>
2023-11-03 02:08:07 +0100

    description -> math_operation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # constants
 
-description = Input('chose math operation (+, -, *, /)')
+math_operation = Input('chose math operation (+, -, *, /)')
 
 # var naming standrts https://www.w3schools.com/python/gloss_python_variable_names.asp#:~:text=A%20variable%20name%20must%20start,AGE%20are%20three%20different%20variables)
 
@@ -10,10 +10,10 @@ float first_number = input("First number: ")
 
 float number_to_add = input("Number to add : ")
 
-if description == "+":
+if math_operation == "+":
     c = a + b
     print("Результат дії дорівнює " + str(c))
 
-elif description = "-":
+elif math_operation = "-":
     c = a - b
     print("Результат дії дорівнює " + str(c))
